c1c03ccc4dc7bb0fb238b6fd52531fd1a7ad4070
HNTQ <quentin_henaut@hotmail.fr>
2022-06-10 14:47:00 +0200

    Feature: add carrousel case and update imageGalery design

--- src/components/Strapi/ImagesGalery/index.js
+++ src/components/Strapi/ImagesGalery/index.js
@@ -1,26 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { Carousel } from "react-responsive-carousel";
 
-const StrapiImageGallery = ({ images }) => {
-  return images ?(
-    <div className={"w-full flex flex-wrap justify-center "}>
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+
+const StrapiImageGallery = ( { images, Affichage } ) => {
+
+  return Affichage==="Carrousel" ? (
+    <Carousel
+      infiniteLoop
+      className="w-full px-2"
+      centerMode
+      centerSlidePercentage={85}
+      statusFormatter={(current, total) => `${current} / ${total}`}
+      transitionTime={800}
+      thumbWidth={80}
+    >
+      {images && images.data.map(image => {
+        return (
+          <div key={`slide-${image.attributes.url}`} className="max-h-160 shadow-normal rounded-md mx-2 mb-2">
+            <img alt="" src={process.env.NEXT_PUBLIC_BACK_URL+image.attributes.url} className="rounded-md shadow-normal object-center object-cover max-h-60 sm:max-h-80 md:max-h-96 lg:max-h-128 xl:max-h-144"/>
+          </div>
+        );
+      })}
+    </Carousel>
+  ) : (
+    <div className={"justify-center grid grid-cols-5 gap-2 xl:gap-4 mx-2"}>
       {images && images.data.map(image => {
         return <img
           key={image.attributes.url}
-          className={"w-64 h-64 object-cover rounded-md shadow-normal m-2"}
+          className={"object-center object-cover rounded-md shadow-normal w-full h-full aspect-square"}
           src={process.env.NEXT_PUBLIC_BACK_URL+image.attributes.url} />;
       }
       )}
-    </div>
-  ):(
-    <div className={"w-full"}>
-      no images
     </div>
   );
 };
 
 StrapiImageGallery.propTypes = {
+  Affichage: PropTypes.string,
   images: PropTypes.shape([])
 };
 
