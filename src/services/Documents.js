f2da4b7f3c9202ed3158a6468e6adff76cbe9106
HNTQ <quentin_henaut@hotmail.fr>
2022-06-17 11:02:32 +0200

    Misc: Update Linter

--- src/services/Documents.js
+++ src/services/Documents.js
@@ -5,8 +5,7 @@ const { NEXT_PUBLIC_API_URL } = process.env;
 class DocumentsServices {
   getByType(type) {
     return new Promise((resolve, reject) => {
-      const url =`${NEXT_PUBLIC_API_URL}/documents?filters[type][unifiedType][$eq]=${type}&populate=*`;
-      axios.get(url)
+      axios.get(`${NEXT_PUBLIC_API_URL}/documents?filters[type][unifiedType][$eq]=${type}&populate=*`)
         .then(({ data }) => resolve(data))
         .catch(reject);
     });
