6135e0b00440d172125b269f4f022cbe1049cb2b
Joshua Vayer <joshua.vayer@gmail.com>
2022-06-21 15:26:11 +0200

    Feature : add description news in card

--- src/components/Display/News/ResultList/index.js
+++ src/components/Display/News/ResultList/index.js
@@ -10,28 +10,31 @@ const NewsResultList = ({ news }) => {
   return (
     <div className="flex flex-col max-w-3xl m-auto gap-4 md:gap-8">
       <h5 className="text-3xl w-full m-auto text-center">Les derniéres actualités</h5>
-      {news.map(({ id, attributes }) => (
-        <Link
-          key={id}
-          href={`/actualites/${attributes.slug}`}
-        >
-          <a>
-            <div className="flex items-center h-[118px] md:h-[200px] rounded shadow hover:shadow-lg hover:cursor-pointer hover:scale-[1.01] transition-transform ">
-              <div className="h-full aspect-square">
-                <img
-                  className="w-full h-full object-cover rounded-l"
-                  src={pathAsAbsolute(fetchFromDataAttribute(attributes.illustration).url)}
-                />
+      {news.map(({ id, attributes }) => {
+        const { title, updatedAt, illustration, slug, description } = attributes;
+        return (
+          <Link
+            key={id}
+            href={`/actualites/${slug}`}
+          >
+            <a>
+              <div className="flex items-center h-[118px] md:h-[200px] rounded shadow hover:shadow-lg hover:cursor-pointer hover:scale-[1.01] transition-transform ">
+                <div className="h-full aspect-square">
+                  <img
+                    className="w-full h-full object-cover rounded-l"
+                    src={pathAsAbsolute(fetchFromDataAttribute(illustration).url)}
+                  />
+                </div>
+                <div className=" flex flex-col justify-center gap-2 p-2 border border-l-0 border-slate-100 h-full">
+                  <h2 className="text-xl font-medium text-actualite">{title}</h2>
+                  <p className="h-[40px] md:h-[53px] text-sm md:text-base overflow-hidden">{description}</p>
+                  <div className="text-sm"><span>Mis à jour le {beautifyDate(updatedAt)}</span></div>
+                </div>
               </div>
-              <div className=" flex flex-col justify-center gap-2 p-2 border border-l-0 border-slate-100 h-full">
-                <h6 className="text-xl font-medium text-actualite">{attributes.title}</h6>
-                <p className="h-[40px] md:h-[53px] text-sm md:text-base overflow-hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisi eu consectetur consectetur, nisl nisi consectetur, nisi nisi consectetur, nisi nisiconsectetur, nisi nisi consectetur, nisi nisi consectetur</p>
-                <div className="text-sm"><span>Mis à jour le {beautifyDate(attributes.updatedAt)}</span></div>
-              </div>
-            </div>
-          </a>
-        </Link>
-      ))}
+            </a>
+          </Link>
+        );}
+      )}
     </div>
   );
 };
