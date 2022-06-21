891b32b36f338645550e3462db5387ba3b87ac26
HNTQ <quentin_henaut@hotmail.fr>
2022-06-21 10:16:00 +0200

    Improvement: responsive design

--- src/components/Display/Person/index.js
+++ src/components/Display/Person/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Person = ({ person }) => {
-
   const { denomination, fonction, photo } = person.attributes;
 
   return (
     <div className="border rounded-md shadow-normal">
-      <img className="object-center object-cover rounded-t-md aspect-square w-full" src={process.env.NEXT_PUBLIC_BACK_URL+photo.data.attributes.url} alt={denomination} />
+      <img
+        className="object-center object-cover rounded-t-md aspect-square w-full"
+        src={process.env.NEXT_PUBLIC_BACK_URL+photo.data.attributes.url} alt={denomination}
+      />
       <div className="px-2 text-center">
         <p>{denomination}</p>
         <p>{fonction}</p>
--- src/components/Strapi/Trombinoscope/index.js
+++ src/components/Strapi/Trombinoscope/index.js
@@ -4,11 +4,11 @@ import PropTypes from "prop-types";
 import Person from "@Display/Person";
 
 const StrapiTrombinoscope = ({ trombinoscopes }) => {
-
   const { data: persons } = trombinoscopes;
+  const trombinoscopeClass = "w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 gap-y-4 lg:gap-y-8 lg:gap-4";
 
   return (
-    <div className=" grid grid-cols-5 gap-4 gap-y-8 pb-5">
+    <div className={trombinoscopeClass}>
       {persons.map(person => (
         <Person key={person.id} person={person} />
       ))}
@@ -19,7 +19,7 @@ const StrapiTrombinoscope = ({ trombinoscopes }) => {
 StrapiTrombinoscope.propTypes = {
   trombinoscopes: PropTypes.shape({
     data: PropTypes.arrayOf().isRequired,
-  }).isRequired,
+  }).isRequired
 };
 
 export default StrapiTrombinoscope;
