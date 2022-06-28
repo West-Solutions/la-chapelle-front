7153a290c689b4c292558db155cebe3b9d5fa55a
HNTQ <quentin_henaut@hotmail.fr>
2022-06-28 18:05:53 +0200

    Fix: Fix for presentation

--- src/components/Strapi/Trombinoscope/index.js
+++ src/components/Strapi/Trombinoscope/index.js
@@ -5,7 +5,7 @@ import Person from "@Display/Person";
 
 const StrapiTrombinoscope = ({ trombinoscopes }) => {
   const { data: persons } = trombinoscopes;
-  const trombinoscopeClass = "w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 gap-y-4 lg:gap-y-8 lg:gap-4";
+  const trombinoscopeClass = "w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 gap-y-4 lg:gap-y-8 lg:gap-4";
 
   return (
     <div className={trombinoscopeClass}>
