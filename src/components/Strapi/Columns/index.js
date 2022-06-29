6357537f4eeef030925643757c75e729e15fd688
HNTQ <quentin_henaut@hotmail.fr>
2022-06-29 11:45:24 +0200

    Improvement: component gaps

--- src/components/Strapi/Columns/index.js
+++ src/components/Strapi/Columns/index.js
@@ -3,16 +3,21 @@ import PropTypes from "prop-types";
 
 import ComponentRenderer from "@Modules/ComponentRenderer";
 
-const StrapiColumns = ({ left, right }) => {
+const StrapiColumns = ({ left, right, sectionColor }) => {
   const renderCol = components => {
     if (!components || !components.length) return null;
-    return  components.map(component => {
-      return <ComponentRenderer key={component.id} component={{ ...component, isColumn: true }} />;
-    });
+    return(
+      <div className="w-full grid grid-cols-1 gap-2">
+        {
+          components.map(component => {
+            return <ComponentRenderer key={component.id} component={{ ...component, isColumn: true, sectionColor }} />;
+          })
+        }
+      </div>);
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-4 md:gap-8">
       <div className="w-full">
         {renderCol(left)}
       </div>
@@ -32,6 +37,7 @@ StrapiColumns.propTypes = {
     id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     __component: PropTypes.string,
   })),
+  sectionColor: PropTypes.string,
 };
 
 export default StrapiColumns;
