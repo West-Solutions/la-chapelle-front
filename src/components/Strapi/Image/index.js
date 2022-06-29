16cd310af195942f4c219416a5007e1d7c04f8b3
HNTQ <quentin_henaut@hotmail.fr>
2022-06-29 17:19:07 +0200

    Feature: Fullscreen on images

--- src/components/Strapi/Image/index.js
+++ src/components/Strapi/Image/index.js
@@ -8,17 +8,28 @@ import { getPosition } from "@StrapiUtils/position";
 
 const StrapiImage = ({ legend, src, showLegend, position }) => {
 
+  const [fullscreen, setFullscreen] = React.useState(false);
+
   const positionValue = getPosition(position);
 
   const imagePosition = positionValue === "left" ? "justify-start" : positionValue === "right" ? "justify-end" : "justify-center";
   return (
     <div className={`w-full flex ${imagePosition}`}>
       <img
-        key={`${src}-${legend}`}
-        className="w-auto rounded-md shadow-normal  max-h-[80vh]"
+        className="w-auto rounded-md shadow-normal cursor-pointer max-h-[80vh]"
         src={pathAsAbsolute(fetchFromDataAttribute(src).url)}
+        onClick={() => setFullscreen(!fullscreen)}
       />
       {showLegend && <p className={"text-center text-gray-600 text-xs mt-2"}>{legend}</p>}
+      <div
+        className={`absolute ${fullscreen ? "block" : "hidden"} z-[9999] w-full left-0 right-0 cursor-pointer -translate-y-1/4`}
+        onClick={() => setFullscreen(!fullscreen)}
+      >
+        <img
+          className="rounded-md shadow-normal mx-auto w-2/3"
+          src={pathAsAbsolute(fetchFromDataAttribute(src).url)}
+        />
+      </div>
     </div>
   );
 };
