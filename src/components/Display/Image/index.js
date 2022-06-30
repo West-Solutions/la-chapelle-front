eea085e7d1d43c3b8a8a1e844f7bcec8a4d4edf4
Quentin Henaut <quentin_henaut@hotmail.fr>
2022-06-30 23:02:18 +0200

    Feature: Fullscreen on images (#41)

---
+++ src/components/Display/Image/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { pathAsAbsolute } from "@Utils/strapi/media";
+import { fetchFromDataAttribute } from "@Utils/strapi/core";
+
+
+const Image = ({ src, imageClassName, containerClassName, isFullscreen, onCloseFullscreen }) => {
+
+  const [fullscreen, setFullscreen] = React.useState(isFullscreen);
+
+
+  const handleFullscreen = () => {
+    if (fullscreen){
+      onCloseFullscreen();
+    }
+
+    setFullscreen(!fullscreen);
+  };
+
+  return (
+    <div className={`w-full h-full ${containerClassName}`}>
+      {!isFullscreen && (
+        <img
+          className={`rounded-md cursor-pointer ${imageClassName}`}
+          src={pathAsAbsolute(fetchFromDataAttribute(src).url)}
+          onClick={handleFullscreen}
+        />
+      )}
+      <div
+        className={`fixed ${fullscreen ? "block" : "hidden"} flex items-center z-[9999] h-screen top-0 right-0 left-0 cursor-pointer bg-zinc-500 bg-opacity-70`}
+        onClick={handleFullscreen}
+      >
+        <img
+          className="rounded-md object-contain mx-auto max-h-[95%] max-w-[95%]"
+          src={pathAsAbsolute(fetchFromDataAttribute(src).url)}
+        />
+      </div>
+    </div>
+  );
+};
+
+Image.propTypes = {
+  containerClassName: PropTypes.string,
+  imageClassName: PropTypes.string,
+  src: PropTypes.shape({
+    data: PropTypes.shape({
+      attributes: PropTypes.shape({
+        url: PropTypes.string
+      })
+    })
+  }),
+  isFullscreen: PropTypes.bool,
+  onCloseFullscreen: PropTypes.func
+};
+
+Image.defaultProps = {
+  containerClassName: "",
+  imageClassName: "",
+  src: {},
+  isFullscreen: false,
+  onCloseFullscreen: () => {}
+};
+
+export default Image;
--- src/components/Strapi/Image/index.js
+++ src/components/Strapi/Image/index.js
@@ -1,24 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { pathAsAbsolute } from "@Utils/strapi/media";
-import { fetchFromDataAttribute } from "@Utils/strapi/core";
-
 import { getPosition } from "@StrapiUtils/position";
 
-const StrapiImage = ({ legend, src, showLegend, position }) => {
+import Image from "@Display/Image";
 
+const StrapiImage = ({ legend, src, showLegend, position }) => {
   const positionValue = getPosition(position);
 
   const imagePosition = positionValue === "left" ? "justify-start" : positionValue === "right" ? "justify-end" : "justify-center";
   return (
-    <div className={`w-full flex ${imagePosition}`}>
-      <img
-        key={`${src}-${legend}`}
-        className="w-auto rounded-md shadow-normal  max-h-[80vh]"
-        src={pathAsAbsolute(fetchFromDataAttribute(src).url)}
-      />
+    <div className="w-full flex flex-col">
+      <Image src={src} containerClassName={`flex ${imagePosition}`} imageClassName="max-h-[80vh] shadow-normal"/>
       {showLegend && <p className={"text-center text-gray-600 text-xs mt-2"}>{legend}</p>}
+
     </div>
   );
 };
