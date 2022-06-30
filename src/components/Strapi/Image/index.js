e272dbf63284e25679b195ae37b2d8effa7c792c
HNTQ <quentin_henaut@hotmail.fr>
2022-06-30 15:22:31 +0200

    Feature: Fullscreen Images

--- src/components/Strapi/Image/index.js
+++ src/components/Strapi/Image/index.js
@@ -1,35 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { pathAsAbsolute } from "@Utils/strapi/media";
-import { fetchFromDataAttribute } from "@Utils/strapi/core";
-
 import { getPosition } from "@StrapiUtils/position";
 
+import Image from "@Display/Image";
+
 const StrapiImage = ({ legend, src, showLegend, position }) => {
-
-  const [fullscreen, setFullscreen] = React.useState(false);
-
   const positionValue = getPosition(position);
 
   const imagePosition = positionValue === "left" ? "justify-start" : positionValue === "right" ? "justify-end" : "justify-center";
   return (
-    <div className={`w-full flex ${imagePosition}`}>
-      <img
-        className="w-auto rounded-md shadow-normal cursor-pointer max-h-[80vh]"
-        src={pathAsAbsolute(fetchFromDataAttribute(src).url)}
-        onClick={() => setFullscreen(!fullscreen)}
-      />
+    <div className="w-full flex flex-col">
+      <Image src={src} containerClassName={`flex ${imagePosition}`} imageClassName="max-h-[80vh] shadow-normal"/>
       {showLegend && <p className={"text-center text-gray-600 text-xs mt-2"}>{legend}</p>}
-      <div
-        className={`absolute ${fullscreen ? "block" : "hidden"} z-[9999] w-full left-0 right-0 cursor-pointer -translate-y-1/4`}
-        onClick={() => setFullscreen(!fullscreen)}
-      >
-        <img
-          className="rounded-md shadow-normal mx-auto w-2/3"
-          src={pathAsAbsolute(fetchFromDataAttribute(src).url)}
-        />
-      </div>
+
     </div>
   );
 };
--- src/components/Strapi/ImagesGalery/index.js
+++ src/components/Strapi/ImagesGalery/index.js
@@ -6,49 +6,68 @@ import { Carousel } from "react-responsive-carousel";
 import { pathAsAbsolute } from "@Utils/strapi/media";
 import { fetchFromDataAttribute } from "@Utils/strapi/core";
 
+import Image from "@Display/Image";
+
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 const StrapiImageGallery = ( { images, Affichage, isColumn } ) => {
 
-  let carrouselClass = "shadow-normal max-h-60 sm:max-h-80 md:max-h-96 lg:max-h-128 xl:max-h-144 rounded-md overflow-hidden mx-2";
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let carrouselClass = "shadow-normal max-h-60 sm:max-h-80 md:max-h-96 lg:max-h-128 xl:max-h-144 rounded-md overflow-hidden mx-2 cursor-pointer";
   let galleryClass = "justify-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 xl:gap-4";
 
   if (isColumn) {
-    carrouselClass= "max-h-60 sm:max-h-80 md:max-h-60 lg:max-h-80 xl:max-h-112 rounded-md overflow-hidden mx-2";
+    carrouselClass= "max-h-60 sm:max-h-80 md:max-h-60 lg:max-h-80 xl:max-h-112 rounded-md overflow-hidden mx-2 cursor-pointer";
     galleryClass= "justify-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 xl:gap-2";
   }
 
   return Affichage === "Carrousel" ? (
-    <Carousel
-      infiniteLoop
-      className="w-full rounded-md overflow-hidden"
-      centerMode
-      centerSlidePercentage={85}
-      statusFormatter={(current, total) => `${current} / ${total}`}
-      transitionTime={300}
-      thumbWidth={80}
-      useKeyboardArrows={true}
-      swipeScrollTolerance={25}
-      showThumbs={false}
-    >
-      {images && images.data.map(image => {
-        return (
-          <div key={`slide-${pathAsAbsolute(fetchFromDataAttribute(image).url)}`} className={carrouselClass}>
-            <img alt="" src={pathAsAbsolute(fetchFromDataAttribute(image).url)} className="object-center object-cover max-h-144"/>
-          </div>
-        );
-      })}
-    </Carousel>
+    <>
+      <Carousel
+        infiniteLoop
+        className="w-full rounded-md overflow-hidden"
+        centerMode
+        centerSlidePercentage={85}
+        statusFormatter={(current, total) => `${current} / ${total}`}
+        transitionTime={300}
+        thumbWidth={80}
+        useKeyboardArrows={true}
+        swipeScrollTolerance={25}
+        showThumbs={false}
+        onClickItem={index => {
+          setSelectedImage(images.data[index]);
+        }}
+      >
+        {images && images.data.map(image => {
+          return (
+            <div key={`slide-${pathAsAbsolute(fetchFromDataAttribute(image).url)}`} className={carrouselClass}>
+              <Image
+                src={image}
+                imageClassName="object-center object-cover max-h-144"/>
+            </div>
+          );
+        })}
+      </Carousel>
+      {selectedImage && (
+        <Image
+          src={selectedImage}
+          imageClassName="object-center object-cover max-h-144"
+          isFullscreen
+          onCloseFullscreen={() => setSelectedImage(null)}
+        />
+      )}
+    </>
   ) : (
     <div className={galleryClass}>
-      {images && images.data.map((image, index) => {
+      {images && images.data.map((image) => {
         return (
           <div key={image.attributes.url} className="relative h-full w-full">
-            <img
-              loading={index <= 4 ? "" : "lazy"}
-              className={"object-center object-cover rounded-md shadow-normal relative w-full h-full aspect-square z-10"}
-              src={pathAsAbsolute(fetchFromDataAttribute(image).url)}
+            <Image
+              src={image}
+              containerClassName="rounded-md shadow-normal aspect-square"
+              imageClassName="relative object-center object-cover aspect-square w-full h-full z-10"
             />
             <div className="absolute top-0 left-0 right-0 bottom-0 rounded-md bg-gray-300 animate-pulse z-0" />
           </div>
