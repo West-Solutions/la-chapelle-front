cc4f83453edb8beee00ab12517974b3cbd755198
HNTQ <quentin_henaut@hotmail.fr>
2022-06-30 23:55:11 +0200

    Improvement: News list design

--- src/components/Display/News/ResultList/index.js
+++ src/components/Display/News/ResultList/index.js
@@ -9,7 +9,6 @@ import { fetchFromDataAttribute } from "@Utils/strapi/core";
 const NewsResultList = ({ news }) => {
   return (
     <div className="flex flex-col max-w-3xl m-auto gap-4 md:gap-8">
-      <h5 className="text-3xl w-full m-auto text-center">Les derniéres actualités</h5>
       {news.map(({ id, attributes }) => {
         const { title, updatedAt, illustration, slug, description } = attributes;
         return (
@@ -17,15 +16,15 @@ const NewsResultList = ({ news }) => {
             key={id}
             href={`/actualites/${slug}`}
           >
-            <a>
-              <div className="flex items-center h-[118px] md:h-[200px] rounded shadow hover:shadow-lg hover:cursor-pointer hover:scale-[1.01] transition-transform ">
+            <a className="w-full rounded shadow-lg hover:shadow-normal hover:cursor-pointer hover:scale-[1.01] transition-transform">
+              <div className="flex items-center h-[118px] md:h-[200px] bg-white">
                 <div className="h-full aspect-square">
                   <img
-                    className="w-full h-full object-cover rounded-l"
+                    className="w-full h-full object-cover rounded-l-md"
                     src={pathAsAbsolute(fetchFromDataAttribute(illustration).url)}
                   />
                 </div>
-                <div className=" flex flex-col justify-center gap-2 p-2 border border-l-0 border-slate-100 h-full">
+                <div className="flex flex-col justify-center gap-2 p-2 border border-l-0 border-slate-100 h-full">
                   <h2 className="text-xl font-medium text-actualite">{title}</h2>
                   <p className="h-[40px] md:h-[53px] text-sm md:text-base overflow-hidden">{description}</p>
                   <div className="text-sm"><span>Mis à jour le {beautifyDate(updatedAt)}</span></div>
