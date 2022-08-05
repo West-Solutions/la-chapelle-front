7b9ca4e05bcf0431c41a0e6e0a0e6a24a9716a81
Joshua Vayer <joshua.vayer@gmail.com>
2022-08-05 07:13:55 +0200

    add fun image

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 const NotFound = () => (
-  <div className="h-full w-full mx-auto text-center pt-8">
+  <div className="h-full container w-full mx-auto text-center pt-8">
     <div className="text-2xl">Error 404</div>
-    <div className="pl-6">Cette page n&apos; exsite pas</div>
+    <div className="pl-6">Cette page n&apos; existe pas</div>
+    <div className="w-full flex justify-end">
+      <img src="https://c.tenor.com/1zi9Ppr4YDsAAAAj/travolta-lost.gif" />
+    </div>
   </div>
 );
 
