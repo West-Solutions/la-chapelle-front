3a38c2c21e5e109bc13e2f2ceacbda4ce49845df
Joshua Vayer <77019747+joshuaVayer@users.noreply.github.com>
2022-08-26 17:40:12 +0200

    Misc : Add 404 fun image (#50)

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Link from "@Components/Controls/Button/Link";
 
 const NotFound = () => (
-  <div className="h-full w-full mx-auto text-center pt-8">
+  <div className="h-full container w-full mx-auto text-center pt-8">
     <div className="text-2xl">Error 404</div>
-    <div className="pl-6">Cette page n&apos; exsite pas</div>
+    <div>Cette page n&apos;existe pas</div>
+    <div className="mt-4">
+      <Link
+        src="/"
+        color="primary"
+        position="center"
+        text="Retour à l'accueil"
+      />
+    </div>
+    <div className="w-full flex justify-end">
+      <img src="https://c.tenor.com/1zi9Ppr4YDsAAAAj/travolta-lost.gif" />
+    </div>
   </div>
 );
 
