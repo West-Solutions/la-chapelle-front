4cef4e5c2786ce5cf3635508cd54dcb5a38f6780
HNTQ <quentin_henaut@hotmail.fr>
2022-08-27 13:01:40 +0200

    Misc: new cache test for static files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: [
+    {
+      key: "cache-control",
+      value: "public, max-age=31536000, must-revalidate"
+    }
+  ]
 };
 
 module.exports = nextConfig;
