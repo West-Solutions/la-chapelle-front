0c2ba8ff6e7853828352c564e2a8776a51d671db
Quentin Henaut <quentin_henaut@hotmail.fr>
2022-08-30 23:18:15 +0200

    Improvement: exclude routes from sitemap and robot.txt (#59)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,28 @@
 /** @type {import('next-sitemap').IConfig} */
+
+const exclude = [
+  "/demo",
+  "/preview/**"
+];
+
+const NEXT_SSG_FILES = [
+  "/*.json$",
+  "/*_buildManifest.js$",
+  "/*_middlewareManifest.js$",
+  "/*_ssgManifest.js$",
+  "/*.js$",
+];
+
 module.exports = {
   siteUrl: "https://www.la-chapelle-aux-filtzmeens.fr/",
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        disallow: NEXT_SSG_FILES,
+      },
+    ],
+  },
+  exclude,
 };
