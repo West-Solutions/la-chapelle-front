dc616c50953ef6015fe844b3549336c3355db052
HNTQ <quentin_henaut@hotmail.fr>
2022-09-03 09:44:24 +0200

    test

--- pages/_app.js
+++ pages/_app.js
@@ -28,7 +28,7 @@ const App = ({ Component, pageProps: page, app }) => {
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          media="print"
+          media="screen"
         />
         <link rel="icon" type="image/png" href={faviconUrl} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
