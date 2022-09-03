c6097f655f809d7193965ec2a3805b473d925e5b
HNTQ <quentin_henaut@hotmail.fr>
2022-09-03 09:40:29 +0200

    improvement: non blocking material icon loading

--- pages/_app.js
+++ pages/_app.js
@@ -28,6 +28,7 @@ const App = ({ Component, pageProps: page, app }) => {
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          media="print"
         />
         <link rel="icon" type="image/png" href={faviconUrl} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
