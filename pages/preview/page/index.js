5c72bd63f82c0f7e091ca8b904e84094538066cf
Quentin Henaut <quentin_henaut@hotmail.fr>
2022-09-07 09:29:33 +0200

    Hotfix : remove cache control (#62)

--- pages/preview/page/index.js
+++ pages/preview/page/index.js
@@ -5,13 +5,9 @@ import PageRenderer from "@Modules/PageRenderer";
 
 const PagePreview = page =>  <PageRenderer {...page} />;
 
-export const getServerSideProps = async ({ query }, res) => {
+export const getServerSideProps = async ({ query }) => {
   const { id } = query;
   const page = await PageServices.get(id);
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=31536000, stale-while-revalidate"
-  );
   return {
     props: { ...page }
   };
